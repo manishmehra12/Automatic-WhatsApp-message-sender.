17ccf8ab281af1f85afc614413cd68b1be15fdcd
manish mehra <157103131+manishmehra12@users.noreply.github.com>
2024-02-23 12:18:07 +0530

    Add files via upload

--- auto whatsapp msg.py
+++ auto whatsapp msg.py
@@ -1,5 +1,5 @@
 import pywhatkit
 
 # Corrected function call
-pywhatkit.sendwhatmsg("+91 7701997744", "This is an automated WhatsApp message tutorial", 21,7)
+pywhatkit.sendwhatmsg("+91 phone number ", "This is an automated WhatsApp message tutorial", 21,7)
 
